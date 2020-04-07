9aa70c7f9676211ebec8b5c8886f9211c06e6e12
David Yang <yzyly1992@gmail.com>
2020-04-07 09:55:52 -0700

    Update JSON and Thumbnails

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, category, dataType, thumb150path, path }) => {
 			<div className='tc bg-white dib br3 pa3 ma2 grow bw2 shadow-5'>
 				<img alt='items' src={thumb150path} />
 				<div>
-					<a href={`file://${path}`} target="_blank"><h2>{name}</h2></a>
+					<a href={`file://${path}`} target="_blank" rel="noopener noreferrer"><h2>{name}</h2></a>
 					<p>{category}, {dataType}</p>
 				</div>
 			</div>
