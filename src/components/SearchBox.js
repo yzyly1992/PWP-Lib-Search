27f012f2409cc30e2a6a057103010e13336c8d00
David Yang <yzyly1992@gmail.com>
2020-04-23 00:04:16 -0700

    Adjust texture json, replace percentage

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
 	return (
-		<div className='pa2'>
+		<div className='pa2 mb3'>
 			<input
 				className='pa3 ba b--green bg-light-green' 
 				type='search' 
-				placeholder='◃┆◉◡◉┆▷' 
+				placeholder='●ω● search ...' 
 				onChange={searchChange}
 			/>
 		</div>
